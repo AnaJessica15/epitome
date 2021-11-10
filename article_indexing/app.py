900728b9268e37727405d4a2a422f670e69aa061
Ana Jessica <anajessica15022002.com>
2021-11-10 15:30:27 +0530

    jina querying working

--- article_indexing/app.py
+++ article_indexing/app.py
@@ -1,62 +1,23 @@
-__version__ = '0.1.0'
+from flask import Flask, render_template, request
+from search import indexing,search_results,prep_docs
 
-import os
-import sys
-
-from jina.flow import Flow
-
-num_docs = int(os.environ.get('MAX_DOCS', 500))
-
-def config():
-    parallel = 2 if sys.argv[1] == 'index' else 1
-    shards = 2
-
-    os.environ['JINA_PARALLEL'] = str(parallel)
-    os.environ['JINA_SHARDS'] = str(shards)
-    os.environ['WORKDIR'] = './workspace'
-    os.makedirs(os.environ['WORKDIR'], exist_ok=True)
-    os.environ['JINA_PORT'] = os.environ.get('JINA_PORT', str(65482))
+app  = Flask(__name__)
 
 
-# for index
+@app.route("/", methods=["GET"])
 def index():
-    f = Flow.load_config('flows/index.yml')
+    return render_template('index.html')
 
-    with f:
-        data_path = os.path.join(os.path.dirname(__file__), os.environ.get('JINA_DATA_PATH',"./data/Article_dataset.csv"))
-        if data_path:
-            f.index_lines(filepath=data_path, batch_size=16, read_mode='r', size=num_docs)
-        else:
-            f.index_lines(lines=['abc', 'cde', 'efg'], batch_size=16, read_mode='r', size=num_docs)
 
-# for search
+@app.route("/summary", methods=["POST"])
 def search():
-    f = Flow.load_config('flows/query.yml')
+    query = request.values.get("keyword")
+    data = search_results(flow, query)
+    return render_template('view.html', data = data)
 
-    with f:
-        f.block()
+    
+if __name__ == "__main__":
+    docs = prep_docs()
+    flow = indexing(docs)
+    app.run(debug = True,host="0.0.0.0")
 
-
-# for test before put into docker
-def dryrun():
-    f = Flow.load_config('flows/query.yml')
-
-    with f:
-        pass
-
-
-if __name__ == '__main__':
-    if len(sys.argv) < 2:
-        print('choose between "index/search/dryrun" mode')
-        exit(1)
-    if sys.argv[1] == 'index':
-        config()
-        index()
-    elif sys.argv[1] == 'search':
-        config()
-        search()
-    elif sys.argv[1] == 'dryrun':
-        config()
-        dryrun()
-    else:
-        raise NotImplementedError(f'unsupported mode {sys.argv[1]}')
